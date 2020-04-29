0579cbcaa0944c1ae6e557dbfbb84818b6f2a5bc
Glitch (amazing-experienced-owl) <none>
2020-04-29 15:31:36 +0000

    👳👳 Updated with Glitch

--- index.js
+++ index.js
@@ -12,4 +12,42 @@ module.exports = (app) => {
     // Post a comment on the issue
     return context.github.issues.createComment(params)
   })
+  
+  app.on(['pull_request.opened', 'pull_request.synchronize'], async context => {
+        const owner = context.payload.repository.owner.login
+        const repo = context.payload.repository.name
+        const number = context.payload.number
+        
+        const comments = []
+        let page = 0
+        while (true) {
+            const files = await context.github.pullRequests.getFiles({
+                owner,
+                repo,
+                number,
+                headers: {accept: 'application/vnd.github.v3.diff'},
+                page,
+                per_page: 100
+            })
+
+            for (const file of files.data) {
+                let contributionType = ""
+                if (file.filename.endsWith('.test.js') || file.filename.endsWith('.test.ts')) {
+                  contributionType = "Tests"
+                } else if (file.filename.endsWith('.js') || file.filename.endsWith('.ts')) {
+                  contributionType = "Code"
+                } else if (file.filename.endsWith('.md') || file.filename.endsWith('.txt')) {
+                  contributionType = "Doc"
+                }
+                const params = context.issue({body: contributionType || 'Hello World!'})
+
+                // Post a comment on the issue
+                return context.github.issues.createComment(params)
+                
+            }
+            page += 1
+
+        }
+        
+    })
 }
