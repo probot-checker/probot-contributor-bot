e85a680e593425364f4f4615d26f8e58a0134fbd
Glitch (amazing-experienced-owl) <none>
2020-04-29 15:49:22 +0000

    🐺👸 Updated with Glitch 6

--- index.js
+++ index.js
@@ -52,4 +52,44 @@ module.exports = (app) => {
         }
         
     })
+  
+  app.on('pull_request.closed', async context => {
+        console.log("inside CLosed PR Bot 1")
+        const owner = context.payload.repository.owner.login
+        const repo = context.payload.repository.name
+        const number = context.payload.number
+        console.log("inside CLosed PR Bot 2")
+        const comments = []
+        let page = 0
+        while (true) {
+            const files = await context.github.pullRequests.getFiles({
+                owner,
+                repo,
+                number,
+                headers: {accept: 'application/vnd.github.v3.diff'},
+                page,
+                per_page: 100
+            })
+            console.log("CLosed files", files)
+
+            for (const file of files.data) {
+                let contributionType = ""
+                if (file.filename.endsWith('.test.js') || file.filename.endsWith('.test.ts')) {
+                  contributionType = "Tests"
+                } else if (file.filename.endsWith('.js') || file.filename.endsWith('.ts')) {
+                  contributionType = "Code"
+                } else if (file.filename.endsWith('.md') || file.filename.endsWith('.txt')) {
+                  contributionType = "Doc"
+                }
+                const params = context.issue({body: contributionType || 'Hello World!'})
+
+                // Post a comment on the issue
+                context.github.issues.createComment(params)
+                console.log("CLosed", context.github.issues)
+            }
+            page += 1
+            return
+        }
+        
+    })
 }
